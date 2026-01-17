dcc08b36b5192fa4e0855050d0bec996e5931c2b
vraj-isotiya <vraj.isotiya@triyanshitechnologies.com>
2026-01-17 09:38:06 +0530

    configure cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const helmet = require("helmet");
 const shipmentRoutes = require("./routes/shipmenturl.routes");
 const orderRoutes = require("./routes/order.routes");
@@ -6,8 +7,20 @@ const { errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 
+const corsOptions = {
+  origin: "https://wilsoninmatepackageprogram.com/",
+  methods: ["GET"],
+};
+
+app.use(cors(corsOptions));
+//app.options("*", cors(corsOptions));
+
 app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
