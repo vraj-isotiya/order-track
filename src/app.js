09fe5e034cc0ace0a0e06f85117a43e3e1a26883
vraj-isotiya <vraj.isotiya@triyanshitechnologies.com>
2026-01-17 10:42:50 +0530

    allow all origin

--- src/app.js
+++ src/app.js
@@ -9,24 +9,8 @@ const app = express();
 
 const allowedOrigins = ["https://wilsoninmatepackageprogram.com"];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow server-to-server or tools like curl/postman
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
-  })
-);
-app.options("*", cors());
+app.use(cors());
+//app.options("*", cors());
 
 app.use(helmet());
 app.use(express.json({ limit: "10kb" }));
